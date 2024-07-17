9c4a192afa0b89832220b55cb78044e4c265e7cc
andyvuong <vuongtrian116@gmail.com>
2024-07-17 15:13:34 -0500

    update corsheaders

--- student_management_server/settings.py
+++ student_management_server/settings.py
@@ -45,16 +45,19 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+
+# To allow credentials (like cookies)
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'student_management_server.urls'
 
